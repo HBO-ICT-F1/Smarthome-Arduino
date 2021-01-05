e8e206008c6d8f053f0ee71154de2c1b03e73ec4
Robert <robertkremer.frl@gmail.com>
2021-01-05 21:44:20 +0100

    Added Ethernet support

--- Smarthome-Arduino.ino
+++ Smarthome-Arduino.ino
@@ -1,38 +1,87 @@
-#include "ESP8266WiFi.h"
- 
-const char* ssid = "";
-const char* password = "";
-WiFiServer wifiServer(6001);
+#ifdef ARDUINO_ARCH_ESP8266
+  #include "ESP8266WiFi.h"
+  
+  #define Server WiFiServer
+  #define Client WiFiClient
+
+
+  // Wifi SSID
+  #define SSID "<ssid here>"
+  
+  // Wifi password
+  #define PASSWORD "<password here>"
+#else
+  #include "SPI.h"
+  #include "Ethernet.h"
+  
+  #define Server EthernetServer
+  #define Client EthernetClient
+
+  byte mac[] = {
+    0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+  };
+  IPAddress ip(192, 168, 1, 177);
+#endif
+
+// Device hostname
+#define HOSTNAME "Smarthome"
+
+// Server port
+#define PORT 6001
+
+// Timeout in seconds
+#define TIMEOUT 2
+
+
+Server server(PORT);
 
 void setup(){
   Serial.begin(115200);
   delay(1000);
- 
-  WiFi.begin(ssid, password);
-  WiFi.hostname("WeMos");
- 
+
+#ifdef ARDUINO_ARCH_ESP8266
+  WiFi.begin(SSID, PASSWORD);
+  WiFi.hostname(HOSTNAME);
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting..");
   }
- 
-  Serial.print("Connected to WiFi. IP:");
+  
+  Serial.print("Connected to WiFi with ip ");
   Serial.println(WiFi.localIP());
-  wifiServer.begin();
+#else
+  Ethernet.init(10);
+  Ethernet.begin(mac, ip);
+
+  if(Ethernet.hardwareStatus() == EthernetNoHardware) {
+    Serial.println("Ethernet shield not found");
+    return;
+  }else if(Ethernet.linkStatus() == LinkOFF) {
+    Serial.println("No Ethernet cable connected");
+    return;
+  }
+  
+  Serial.print("Connected to Ethernet with ip ");
+  Serial.println(Ethernet.localIP());
+#endif
+ 
+  server.begin();
 }
  
 void loop(){
-  WiFiClient client = wifiServer.available();
+  Client client = server.available();
   if (!client) return;
   Serial.println("Client connected");
-  for(uint8_t i = 0; i < 40 && client.connected(); i++){ // 2 Seconds
+  
+  for(uint8_t i = 0; i < TIMEOUT * 20 && client.connected(); i++){
     String data = "";
     
     while (client.available() > 0) {
       char c = client.read();        
       data.concat(c);
     }
-       client.println("Hallo");
+    
     if(data != ""){
       data.trim(); 
       if(data == "example"){
@@ -40,6 +89,7 @@ void loop(){
         break;
       }
     }
+    
     delay(50);
   }
   
